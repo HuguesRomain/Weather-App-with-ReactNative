b8b9a0a904fefd0936171bf7d23653465b331b77
HuguesRomain <huguesromainpro@gmail.com>
2019-02-19 00:37:48 +0100

    input

--- components/Search.js
+++ components/Search.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default class Home extends React.Component{
+  constructor (props){
+    super(props)
+    this.state = {
+      city: 'Paris',
+    }
+  }
+  
+  setCity (city){
+    this.setState({city})
+  }
+
   render() {
     return(
        <TextInput
         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-
+        value={this.state.city}
+        onChangeText={(text) => this.setCity(text)}
       />
     );
   }
