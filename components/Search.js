8d4738a52ee722fe1333f606bbee04cbbbe9e01c
HuguesRomain <huguesromainpro@gmail.com>
2019-02-19 00:22:43 +0100

    first native compotent

---
+++ components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
+
+export default class Home extends React.Component{
+  render() {
+    return(
+       <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+
+      />
+    );
+  }
+}
--- components/about.js
+++ components/about.js
@@ -1,13 +1,27 @@
 import * as React from 'react';
-import { Text, View, Fragment, StyleSheet, Image } from 'react-native';
+import { Text, View, Fragment, StyleSheet, Image, ActivityIndicator } from 'react-native';
  
  export default class About extends React.Component {
    render(){
      return(
-       <View> 
-       <Text> A propos de moi</Text>
-       <Text> lorem ipsum  </Text>
+       <View style={style.view}> 
+       <Text style={style.title}> A propos de moi</Text>
+       <Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </Text>
+      {/*  <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+       <ActivityIndicator style={style.view} color="#FF0000" size="small" animating={true}/>
+       <ActivityIndicator style={style.view} color="#FF0000" size="small" animating={true}/>
+       </View> */}
        </View>
      );
    }
  }
+
+const style = StyleSheet.create({
+  view: {
+    margin: 20,
+  },
+  title: {
+    fontSize: 22,
+    marginBottom: 20
+  }
+})
