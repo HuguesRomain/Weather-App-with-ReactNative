e37a482cd36c44f28635abc901ad84ab3cb28263
HuguesRomain <huguesromainpro@gmail.com>
2019-02-18 23:46:22 +0100

    second commit

---
+++ components/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text, View, Fragment, StyleSheet, Image } from 'react-native';
+ 
+ export default class About extends React.Component {
+   render(){
+     return(
+       <View> 
+       <Text> A propos de moi</Text>
+       <Text> lorem ipsum  </Text>
+       </View>
+     );
+   }
+ }
