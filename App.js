a35f275b5b96a588ce60b2f163df89ca475e9c9b
HuguesRomain <huguesromainpro@gmail.com>
2019-02-20 23:46:53 +0100

    first navigation test

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import About from './components/About';
+import Search from './components/Search';
 import Home from './components/Search';
+import { TabNavigator } from './node_modules/react-navigation';
+
+const Tabs = TabNavigator({
+  Search: Search, 
+  About: About,
+}); 
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{marginHorizontal: 40, marginVertical: 40}}>
-      <Home/>
-      </View>
+      <Tabs />
     );
   }
 }
