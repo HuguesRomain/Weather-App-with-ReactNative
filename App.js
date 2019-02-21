a7a62e347f1f7c4222b246a3f0aab1f29b79c065
HuguesRomain <huguesromainpro@gmail.com>
2019-02-21 23:47:41 +0100

    description

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import About from './components/About';
 import Search from './components/Search';
-import Home from './components/Search';
 import { TabNavigator } from './node_modules/react-navigation';
 
 const Tabs = TabNavigator({
@@ -12,7 +11,7 @@ const Tabs = TabNavigator({
 
 export default class App extends React.Component {
   render() {
-    return (
+    return ( 
       <Tabs />
     );
   }
